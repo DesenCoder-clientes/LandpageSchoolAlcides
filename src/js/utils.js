3f582a694365ec37866d4ff3b9fd3f1f968d14b2
leopousa_ <leandro.sousa@beneagis.com>
2020-10-03 13:47:11 -0300

    update scroll top fixed

--- src/js/utils.js
+++ src/js/utils.js
@@ -10,3 +10,16 @@ selectorList.forEach((item,index)=>{
   })
 
 })
+
+window.onscroll = function() {scrollMenuFixed()};
+
+var header = document.getElementById('header');
+var sticky = header.offsetTop;
+
+function scrollMenuFixed() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
