20701df7cc40a1f461442f505dd56eff8dbdb123
leopousa_ <leandro.sousa@beneagis.com>
2020-10-18 18:51:06 -0300

    incators carousel

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -1,15 +1,20 @@
 let time = 5000;
 let currentImageIndex = 0;
 let images = document.querySelectorAll("#carousel img");
+let indicator = document.querySelectorAll("#contentIndicators div");
 let max = images.length;
 let next = document.getElementById("next");
 let prev = document.getElementById("prev");
 
+console.log(indicator.length);
+
 function nextImage() {
   images[currentImageIndex].classList.remove("selected");
+  indicator[currentImageIndex].classList.remove("actived");
   currentImageIndex++;
   if (currentImageIndex >= max) currentImageIndex = 0;
   images[currentImageIndex].classList.add("selected");
+  indicator[currentImageIndex].classList.add("actived");
 }
 
 function start() {
@@ -20,17 +25,21 @@ function start() {
 
 next.onclick = function () {
   images[currentImageIndex].classList.remove("selected");
+  indicator[currentImageIndex].classList.remove("actived");
   currentImageIndex++;
   if (currentImageIndex >= max) currentImageIndex = 0;
   images[currentImageIndex].classList.add("selected");
+  indicator[currentImageIndex].classList.add("actived");
   console.log("next");
 };
 
 prev.onclick = function () {
   images[currentImageIndex].classList.remove("selected");
+  indicator[currentImageIndex].classList.remove("actived");
   currentImageIndex--;
   if (currentImageIndex <= 0) currentImageIndex = 3;
   images[currentImageIndex].classList.add("selected");
+  indicator[currentImageIndex].classList.add("actived");
   console.log("prev");
 };
 
