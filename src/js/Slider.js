f94aef86ba5d46a70baf3ad8b1ea9ae662a3a153
leopousa_ <leandro.sousa@beneagis.com>
2020-10-18 18:30:28 -0300

    add buttons carousel

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -1,23 +1,37 @@
-let time = 5000,
-  currentImageIndex = 0,
-  images = document.querySelectorAll("#carousel img");
-max = images.length;
+let time = 5000;
+let currentImageIndex = 0;
+let images = document.querySelectorAll("#carousel img");
+let max = images.length;
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
 
 function nextImage() {
   images[currentImageIndex].classList.remove("selected");
-
   currentImageIndex++;
-
   if (currentImageIndex >= max) currentImageIndex = 0;
-
   images[currentImageIndex].classList.add("selected");
 }
 
 function start() {
   setInterval(() => {
-    // troca de image
     nextImage();
   }, time);
 }
+
+next.onclick = function () {
+  images[currentImageIndex].classList.remove("selected");
+  currentImageIndex++;
+  if (currentImageIndex >= max) currentImageIndex = 0;
+  images[currentImageIndex].classList.add("selected");
+  console.log("next");
+};
+
+prev.onclick = function () {
+  images[currentImageIndex].classList.remove("selected");
+  currentImageIndex--;
+  if (currentImageIndex <= 0) currentImageIndex = 3;
+  images[currentImageIndex].classList.add("selected");
+  console.log("prev");
+};
 
 window.addEventListener("load", start);
